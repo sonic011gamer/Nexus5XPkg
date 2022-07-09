49a196a4a83f42236a383d040228416ff00e73c5
Dominik Kobinski <dominikkobinski314@gmail.com>
2022-07-09 12:11:35 +0200

    PrePi: Add modified code from SurfaceDuoPkg
    Use the Surface Duo implementation since it has relocation. Leave
    EarlyQGic from the original one.
    
    Thanks to Gustave Monce for explaining it all

--- PrePi/Pi.c
+++ PrePi/Pi.c
@@ -1,14 +1,12 @@
 // Pi.c: Entry point for SEC(Security).
 
 #include "Pi.h"
-#include "HvcPatch.h"
 
 #include <Pi/PiBootMode.h>
 #include <Pi/PiHob.h>
 #include <PiDxe.h>
 #include <PiPei.h>
 
-#include <Configuration/Hob.h>
 #include <Guid/LzmaDecompress.h>
 #include <Ppi/GuidedSectionExtraction.h>
 
@@ -17,7 +15,6 @@
 #include <Library/CacheMaintenanceLib.h>
 #include <Library/DebugAgentLib.h>
 #include <Library/DebugLib.h>
-#include <Library/FrameBufferSerialPortLib.h>
 #include <Library/HobLib.h>
 #include <Library/IoLib.h>
 #include <Library/MemoryAllocationLib.h>
@@ -28,63 +25,27 @@
 #include <Library/PrePiLib.h>
 #include <Library/SerialPortLib.h>
 
-#include <IndustryStandard/ArmStdSmc.h>
-#include <Library/ArmHvcLib.h>
-#include <Library/ArmSmcLib.h>
-
 VOID EFIAPI ProcessLibraryConstructorList(VOID);
 
 STATIC VOID UartInit(VOID)
 {
   SerialPortInitialize();
 
-  DEBUG((EFI_D_INFO, "\nTianoCore on 950XL (AArch64)\n"));
+  DEBUG((EFI_D_INFO, "\nTianoCore on Nexus 5X (AArch64)\n"));
   DEBUG(
       (EFI_D_INFO, "Firmware version %s built %a %a\n\n",
        (CHAR16 *)PcdGetPtr(PcdFirmwareVersionString), __TIME__, __DATE__));
 }
 
-STATIC VOID PsciFixupInit(VOID)
+STATIC VOID Reboot(VOID)
 {
-  EFI_PHYSICAL_ADDRESS WakeFromPowerGatePatchOffset;
-  EFI_PHYSICAL_ADDRESS LowerELSynchronous64PatchOffset;
-  EFI_PHYSICAL_ADDRESS LowerELSynchronous32PatchOffset;
-  ARM_HVC_ARGS         StubArgsHvc;
-  ARM_SMC_ARGS         StubArgsSmc;
-
-  WakeFromPowerGatePatchOffset    = WAKE_FROM_POWERGATE_PATCH_ADDR;
-  LowerELSynchronous64PatchOffset = LOWER_EL_SYNC_EXC_64B_PATCH_ADDR;
-  LowerELSynchronous32PatchOffset = LOWER_EL_SYNC_EXC_32B_PATCH_ADDR;
-
-  CopyMem(
-      (VOID *)WakeFromPowerGatePatchOffset, WakeFromPowerGatePatchHandler,
-      sizeof(WakeFromPowerGatePatchHandler));
-  CopyMem(
-      (VOID *)LowerELSynchronous64PatchOffset, LowerELSynchronous64PatchHandler,
-      sizeof(LowerELSynchronous64PatchHandler));
-  CopyMem(
-      (VOID *)LowerELSynchronous32PatchOffset, LowerELSynchronous32PatchHandler,
-      sizeof(LowerELSynchronous32PatchHandler));
-
-  ArmDataSynchronizationBarrier();
-  ArmInvalidateDataCache();
-  ArmInvalidateInstructionCache();
-
-  // Call into the handler to make HCR_EL2.TSC sticky
-  StubArgsHvc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
-  ArmCallHvc(&StubArgsHvc);
-
-  // Well...
-  StubArgsSmc.Arg0 = ARM_SMC_ID_PSCI_VERSION;
-  ArmCallSmc(&StubArgsSmc);
+  MmioWrite32(0xfc4ab000, 0);
 }
 
-VOID Main(IN VOID *StackBase, IN UINTN StackSize, IN UINT64 StartTimeStamp)
+VOID Main(IN VOID *StackBase, IN UINTN StackSize)
 {
 
   EFI_HOB_HANDOFF_INFO_TABLE *HobList;
-  PRELOADER_ENVIRONMENT *     PreEnv = (VOID *)PRELOADER_ENV_ADDR;
-  UINT32                      Crc32  = 0;
   EFI_STATUS                  Status;
 
   UINTN MemoryBase     = 0;
@@ -92,8 +53,13 @@ VOID Main(IN VOID *StackBase, IN UINTN StackSize, IN UINT64 StartTimeStamp)
   UINTN UefiMemoryBase = 0;
   UINTN UefiMemorySize = 0;
 
-  // PSCI fixup init
-  PsciFixupInit();
+#if USE_MEMORY_FOR_SERIAL_OUTPUT == 1
+  // Clear PStore area
+  UINT8 *base = (UINT8 *)0x1ff00000ull;
+  for (UINTN i = 0; i < 0x40000; i++) {
+    base[i] = 0;
+  }
+#endif
 
   // Architecture-specific initialization
   // Enable Floating Point
@@ -153,7 +119,7 @@ VOID Main(IN VOID *StackBase, IN UINTN StackSize, IN UINT64 StartTimeStamp)
   BuildStackHob((UINTN)StackBase, StackSize);
 
   // TODO: Call CpuPei as a library
-  BuildCpuHob(40, PcdGet8(PcdPrePiCpuIoSize));
+  BuildCpuHob(ArmGetPhysicalAddressBits(), PcdGet8(PcdPrePiCpuIoSize));
 
   // Set the Boot Mode
   SetBootMode(BOOT_WITH_FULL_CONFIGURATION);
@@ -162,20 +128,8 @@ VOID Main(IN VOID *StackBase, IN UINTN StackSize, IN UINT64 StartTimeStamp)
   Status = PlatformPeim();
   ASSERT_EFI_ERROR(Status);
 
-  // Initialize Platform PreLoader HOBs
-  if (PreEnv->Header == PRELOADER_HEADER) {
-    Crc32         = PreEnv->Crc32;
-    PreEnv->Crc32 = 0x0;
-    if (CalculateCrc32(PreEnv, sizeof(PRELOADER_ENVIRONMENT)) == Crc32) {
-      PreEnv->Crc32 = Crc32;
-      DEBUG((EFI_D_INFO, "CRC32 check succeeded \n"));
-    }
-    else {
-      // Hey we have memory corrpution
-      DEBUG((EFI_D_ERROR, "CRC32 check failed \n"));
-      ASSERT(FALSE);
-    }
-  }
+  // Install SoC driver HOBs
+  //InstallPlatformHob();
 
   // Now, the HOB List has been initialized, we can register performance
   // information PERF_START (NULL, "PEI", NULL, StartTimeStamp);
@@ -198,5 +152,5 @@ VOID Main(IN VOID *StackBase, IN UINTN StackSize, IN UINT64 StartTimeStamp)
 
 VOID CEntryPoint(IN VOID *StackBase, IN UINTN StackSize)
 {
-  Main(StackBase, StackSize, 0);
+  Main(StackBase, StackSize);
 }
