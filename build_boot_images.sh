6c2de901cb88d7f38e4a52419ea2cfdefde53d40
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-10-09 16:09:59 +0300

    Try and fix upload path

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -10,3 +10,4 @@ gzip -c < Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/BootShimMSM8992_EFI.fd >Build/Nexu
 cat ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/BootShimMSM8992_EFI.fd.gz ./Nexus5XPkg/device-specific/sm6225-redmi-note-11-spesn.dtb >> Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/Image.gz-dtb
 
 mkbootimg --kernel ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/Image.gz-dtb --ramdisk ./Nexus5XPkg/device-specific/ramdisk-null --base 0x40000000 --pagesize 4096 -o ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/uefi.img 
+cp ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/uefi.img ../Nexus5XPkg/uefi.img
