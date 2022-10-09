46a5d802427d37caa5b2e18064038a62ccde7c45
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-10-09 16:29:49 +0300

    Modify the uefi.img creation

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,13 +1,15 @@
 # Create uefi.img
-cd ..
-cd edk2
+cat ./BootShim/BootShim.bin ../edk2/Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/MSM8992_EFI.fd ./device-specific/dummykernel > ./device-specific/bootpayload.bin
 
-cat ./Nexus5XPkg/BootShim/BootShim.bin ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/MSM8992_EFI.fd > ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/BootShimMSM8992_EFI.fd
-
-gzip -c < Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/BootShimMSM8992_EFI.fd >Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/BootShimMSM8992_EFI.fd.gz
-
-# Rev 1.0
-cat ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/BootShimMSM8992_EFI.fd.gz ./Nexus5XPkg/device-specific/sm6225-redmi-note-11-spesn.dtb >> Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/Image.gz-dtb
-
-mkbootimg --kernel ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/Image.gz-dtb --ramdisk ./Nexus5XPkg/device-specific/ramdisk-null --base 0x40000000 --pagesize 4096 -o ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/uefi.img 
-cp ./Build/Nexus5X-AARCH64/DEBUG_GCC5/FV/uefi.img ../Nexus5XPkg/uefi.img
+mkbootimg \
+  --kernel ./device-specific/bootpayload.bin \
+  --ramdisk ./device-specific/ramdisk-null \
+  -o ./uefi.img \
+  --pagesize 4096 \
+  --header_version 2 \
+  --cmdline "console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=surfaceduo androidboot.hardware.platform=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 kpti=off buildvariant=user" \
+  --dtb ./device-specific/sm6225-redmi-note-11-spesn.dtb \
+  --base 0x0 \
+  --os_version 12.0.0 \
+  --os_patch_level 2022-08-01 \
+  --second_offset 0xf00000
